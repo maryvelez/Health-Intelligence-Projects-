b5a51b34787375efe5b1963208b41d040bde0a63
maryvelez <marychrisvelez@gmail.com>
2024-02-16 09:36:51 -0500

    Create Project_001_code.py
    Input your temperature into the project and find out if it is normal.

---
+++ Project_001_code.py
@@ -0,0 +1,33 @@
+#import libaries
+
+#Define any variables / get input from user
+#temp
+temp = int(input("What is your temp?: "))
+if temp > (37.3):
+     print("You have a fever")
+else:
+    print("Your temperature is normal")
+#symp
+#age
+#BMI
+#allergies
+
+#Description of Health assessment I am ready to start my weekly health assessment + prompt user to type name for confirmation/check in
+
+#Hello "name" welcome do your weekly health assesment, Please fill out the followung form with the values
+
+#print variables above to get that input
+
+#shit ton of If, elif, else statements for each to compare numbers
+
+#import a document with the values diagnostic temp etc
+
+
+# run data though doc = if input line - value of key on doc , print key (key-value pairs)
+
+#print possible illnesses, matches
+
+#if no match, print they are healthy, UNLESS 1 variable is not in range, then suggest nmedical profesisonal 
+
+
+
