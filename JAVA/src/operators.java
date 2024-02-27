d0cc99802a621af25077e44b757eee08d6114964
maryvelez <marychrisvelez@gmail.com>
2024-02-27 12:50:19 -0500

    updates!

---
+++ JAVA/src/operators.java
@@ -0,0 +1,23 @@
+public class operators {
+    public static void main(String[] args) {
+        int number1 = 12;
+        int number2 = 6;
+
+        //addition
+        System.out.println(number1 + number2);
+
+        // Subtraction
+        System.out.println(number1 - number2);
+
+        //multiplication
+        System.out.println(number1 *number2);
+
+        //division
+        System.out.println(number1/number2);
+
+        //remainder (modulo/modulus)
+        System.out.println(number1 % number2);
+        
+
+    }
+}
